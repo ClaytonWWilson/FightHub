4eeda2e474c695f9e23cecc5c8df3c260e153abc
Ubuntu <claytonwilson@protonmail.com>
2019-04-20 03:19:40 -0400

    upload.php html and temporary style

--- upload.php
+++ upload.php
@@ -14,11 +14,44 @@
     </form>
 
     <!-- submit.php will read the variables from the url and add it to the database -->
-    <form class="upload-form" action="submit.php">
-        <input type="text" name="url">
-        <input type="text" name="name">
-        <textarea name="description" id="" cols="30" rows="10"></textarea>
-        <textarea name="tags" cols="30" rows="10"></textarea>
+    <form class="upload-form" onsubmit="validateUpload()">
+
+        <h3>Youtube URL *</h3>
+        <input type="text" name="url" id="url">
+
+        <h3>Name *</h3>
+        <input type="text" name="name" id="name">
+        
+        <h3>Username *</h3>
+        <input type="text" name="username">
+        
+        <h3>Description</h3>
+        <textarea name="description" cols="30" rows="10"></textarea>
+        
+        <h3>Event Date</h3>
+        <input type="date" name="video-date">
+        
+        <h3>Video Length</h3>
+        <input type="text" name="length">
+        
+        <h3>Characters (Seperated by commas) *</h3>
+        <input type="text" name="characters">
+        
+        <h3>Players (Seperated by commas) *</h3>
+        <input type="text" name="players">
+        
+        <h3>Select Game *</h3>
+        <select name="game">
+            <option disabled selected value>-- Game --</option>
+            <option value="Street Fighter">Street Fighter</option>
+            <option value="Mortal Kombat">Mortal Kombat</option>
+            <option value="Super Smash Bros">Super Smash Bros</option>
+        </select>
+        
+        <h3>Stage Name *</h3>
+        <input type="text" name="stage">
+        
+        
         <input type="submit" name="submit" value="Submit">
     </form>
 
