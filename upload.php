9b4d874b62172f9c951d1e60cf02c1d326647f98
Ubuntu <tcecil@purdue.edu>
2019-04-20 16:51:49 -0400

    fixed upload.php merge conflict

--- upload.php
+++ upload.php
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-<html>
-<head>
-    <title>FightHub - Upload</title>
-    <link rel="stylesheet" href="main.css">
-    <script src="scripts/main.js"></script>
-</head>
-
-<body>
-	<?php
-	$servername = "localhost";
-	$username = "fighthubuser";
-	$password = "cC33dC8W";
-
-	$conn = new mysqli($servername, $username, $password);
-
-	if($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-
-	$url = $_GET['url'];
-	$name = $_GET['name'];
-	$username = $_GET['username'];
-	$description = $_GET['description'];
-	$event_date = $_GET['date'];
-	$length = $_GET['length'];
-	$characters = $_GET['characters'];
-	$players = $_GET['players'];
-	$game = $_GET['game'];
-	$stage = $_GET['stage'];
-	
-	echo "<h1>" . $game . "</h1>";
-	?>
-</body>
-</html>
-=======
 <html>
 <head>
     <title>FightHub - Upload</title>
@@ -97,4 +61,3 @@
 
 </body>
 </html>
->>>>>>> master
