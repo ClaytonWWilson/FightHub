bc2c6484092d66753aeb9d698b01a25664cfc531
Ubuntu <claytonwilson@protonmail.com>
2019-04-23 01:22:54 -0400

    Added English tags and a link to the index page on the logo.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="en">
 <head>
     <title>FightHub - Home</title>
     <link rel="stylesheet" href="main.css">
@@ -6,7 +6,7 @@
     <body>
         <!-- Titlebar -->
         <form class="search" action="search.php">
-            <h1 class="title-text">FightHub</h1>
+            <a href="/"><h1 class="title-text">FightHub</h1></a>
             <input type="text" name="query" class="search" placeholder="Search..">
             <input type="submit" name="submit" class="submit" value="Search">
             <a href="upload.php"><img src="images/upload.png" class="upload" alt="upload"></a>
